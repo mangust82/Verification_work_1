7b7c03d9c891f036f141ac4d6a7e62b691500f23
Eugen <mangust82@gmail.com>
2022-11-25 02:16:17 +0300

    task and enter of array

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу которая из имеющегося массива из строк сформирует массив из строк, длина котрых менше 
+// либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+string[] TestMas = new string[] {"hhhgj", "ghgghgjhghjg", "hj", "780", "jhkjhkjkj", "u"};
+
+Console.WriteLine($"[{String.Join(",", TestMas)}]");
+ThreeMas(TestMas);
+Console.WriteLine($"[{String.Join(",", ThreeMas(TestMas))}]");
+
